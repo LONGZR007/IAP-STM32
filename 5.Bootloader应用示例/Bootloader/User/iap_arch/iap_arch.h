443f5e73ae817f5f5c7cc5894096c11513ce7ff1
LONGZR007 <1558193230@qq.com>
2019-12-12 22:55:09 +0800

    怎么BootLoader应用示例

---
+++ 5.Bootloader应用示例/Bootloader/User/iap_arch/iap_arch.h
@@ -0,0 +1,14 @@
+#ifndef __IAP_ARCH_H
+#define	__IAP_ARCH_H
+
+#include "stm32f4xx.h"
+
+#define     RECEIVE_TIMEOUT    0x1FFFFFF
+
+int x_receive(__IO uint8_t *data, uint32_t len);
+int x_transmit_ch(uint8_t ch);
+uint32_t x_receive_flash_erasure(uint32_t address);
+int x_receive_flash_writea(uint32_t start_address, const void *data, uint32_t len);
+
+#endif /* __IAP_ARCH_H */
+
